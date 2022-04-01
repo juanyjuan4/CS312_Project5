4338b909058bd26dcd8fc371e821d335ca1dc18a
juanyjuan4 <73352285+juanyjuan4@users.noreply.github.com>
2022-03-31 21:53:56 -0600

    Third Commit
    I ironed out the bugs and made the code work. The lab is finished.

--- TSPSolver.py
+++ TSPSolver.py
@@ -18,12 +18,14 @@ import numpy as np
 from TSPClasses import *
 import heapq
 import itertools
+import uuid
 
 
 
 class TSPSolver:
 	def __init__( self, gui_view ):
 		self._scenario = None
+		self.bssf = None
 
 	def setupWithScenario( self, scenario ):
 		self._scenario = scenario
@@ -77,11 +79,13 @@ class TSPSolver:
 		matrix = np.empty((ncities, ncities))
 		matrix.fill(np.inf)
 		
+		# Add the path distances into the matrix.
 		for i in range(0, ncities):
 			for j in range(0, ncities):
 				if self._scenario._edge_exists[i, j]:
 					matrix[i, j] = self._scenario._cities[i].costTo(self._scenario._cities[j])
 		
+		# Reduce the matrix if needed.
 		if reduce:
 			rowMins = np.amin(matrix, axis = 1)
 			for i in range(0, ncities):
@@ -109,47 +113,43 @@ class TSPSolver:
 		algorithm</returns>
 	'''
 
+	# This is used as my initial BSSF.
 	def greedy( self,time_allowance=60.0 ):
 		start_time = time.time()
 		results = {}
 		ncities = len(self._scenario._cities)
 		matrix, lowerBound = self.createMatrix(False)
-		count = 0
-		print(matrix)
+		total = 1
 		
 		currentIndex = 0
 		path = []
 		path.append(self._scenario._cities[currentIndex])
+		# While not all cities have been visited, this algorithm finds the next shortest path,
+		# and sets unavailable paths to infinity.
 		while len(path) < ncities:
 			matrix[currentIndex, 0] = np.inf
 			nextMin = np.amin(matrix[currentIndex, :])
-			print(nextMin)
 			lowerBound += nextMin
 			nextIndex = np.where(matrix[currentIndex, :] == nextMin)[0][0]
 			path.append(self._scenario._cities[nextIndex])
 			matrix[:, nextIndex] += np.repeat(np.inf, ncities)
 			matrix[currentIndex, :] += np.repeat(np.inf, ncities)
 			currentIndex = nextIndex
-			count += 1
-			print(matrix)
+			total += 1
 		
-		print(matrix[currentIndex, 0])
-		if matrix[currentIndex, 0] == np.inf:
-			return np.inf
+		if matrix[currentIndex, 0] == np.inf: # Check if the path is viable or not.
+			self.bssf = TSPSolution([self._scenario._cities[0], self._scenario._cities[0]])
 		else:
-			path.append(self._scenario._cities[0])
+			self.bssf = TSPSolution(path)
 		
-		self.bssf = TSPSolution(path)
-		self.bssf._costOfRoute()
-		print(self.bssf.cost)
 		end_time = time.time()
 		
 		results['cost'] = self.bssf.cost
 		results['time'] = end_time - start_time
-		results['count'] = count
+		results['count'] = None
 		results['soln'] = self.bssf
 		results['max'] = None
-		results['total'] = None
+		results['total'] = total
 		results['pruned'] = None
 		return results
 
@@ -169,86 +169,99 @@ class TSPSolver:
 		results = {}
 		ncities = len(self._scenario._cities)
 		matrix, lowerBound = self.createMatrix(True)
-		print("Number of cities:")
-		print(ncities)
-		print("Lower bound:")
-		print(lowerBound)
-		print("Original Matrix:")
-		print(matrix)
 		count = 0
 		pruned = 0
+		total = 1
 		
-		self.greedy()
+		self.greedy() # Run the greedy algorithm to fill self.bssf for use later.
 		
 		heap = []
+		hashMap = {} # Used to store other data from the priority queue that I didn't want messing up the ordering.
+		maxHeapSize = 0
 		currentIndex = 0
 		path = []
 		path.append(self._scenario._cities[currentIndex])
 		for i in range(1, ncities):
-			count += 1
+			total += 1
 			tempBound = lowerBound + matrix[currentIndex, i]
 			tempMatrix = matrix.copy()
-			#rowMins = np.amin(tempMatrix, axis = 1)
 			tempMatrix[:, i] += np.repeat(np.inf, ncities)
 			tempMatrix[currentIndex, :] += np.repeat(np.inf, ncities)
-			#print("Temp Matrix:", i)
-			#print(tempMatrix)
 			rowMins = np.amin(tempMatrix, axis = 1)
-			#print(rowMins)
 			tempPath = path + [self._scenario._cities[i]]
+   
 			for j in range(0, ncities):
 				if rowMins[j] == np.inf:
 					continue
 				tempMatrix[j, :] -= np.repeat(rowMins[j], ncities)
 				tempBound += rowMins[j]
-			#print(rowMins)
-			#print(tempBound)
-			if tempBound < 9000:
-				heapq.heappush(heap, (tempBound, i, tempMatrix, tempPath))
-				#print(tempMatrix)
+    
+			if tempBound < self.bssf.cost:
+				tempID = str(uuid.uuid1()) # Create an ID for hashing data.
+				hashMap[tempID] = (i, tempMatrix.copy(), tempPath) # Add important data to hashmap.
+				heapq.heappush(heap, (tempBound, tempID)) # Add current path cost to queue, with associated hash.
+				if maxHeapSize < len(heap):
+					maxHeapSize = len(heap)
+			else:
+				pruned += 1
 		
+		# Expand values from the queue until best path found or time runs out.
+		# This code is very similar to the block above.
 		while heap and time.time() - start_time < time_allowance:
-			print(heap[0])
-			print(len(heap))
-			currentBound, currentIndex, currentMatrix, currentPath = heapq.heappop(heap)
+			currentBound, currentID = heapq.heappop(heap) # Pop off the queue.
+			currentIndex, currentMatrix, currentPath = hashMap[currentID] # Retrieve data from the hash table.
+			if currentBound > self.bssf.cost:
+				pruned += 1
+				continue
+   
 			for i in range(0, ncities):
-				if i == currentIndex:
+				if i == currentIndex: # No need to check paths to itself.
 					continue
-				count += 1
+				if i == 0 and len(currentPath) != ncities: # If it's not at the end, don't check the first location.
+					continue
+				total += 1
 				tempBound = currentBound + currentMatrix[currentIndex, i]
 				tempMatrix = currentMatrix.copy()
-				#rowMins = np.amin(tempMatrix, axis = 1)
+				# Infinite out blocked paths, and find the minimum values from each row.
 				tempMatrix[:, i] += np.repeat(np.inf, ncities)
 				tempMatrix[currentIndex, :] += np.repeat(np.inf, ncities)
-				#for j in range(0, ncities):
-				#	tempMatrix[j, i] = np.inf
-				#	tempMatrix[currentIndex, j] = np.inf
 				rowMins = np.amin(tempMatrix, axis = 1)
-				#print(rowMins)
+				# Add to the path.
 				tempPath = currentPath + [self._scenario._cities[i]]
+    
 				for j in range(0, ncities):
 					if rowMins[j] == np.inf:
 						continue
 					tempMatrix[j, :] -= np.repeat(rowMins[j], ncities)
 					tempBound += rowMins[j]
-				#print(tempBound)
-				if tempBound < 9000:
-					#print(tempMatrix)
-					if i == 0:
+     
+				if tempBound < self.bssf.cost:
+					if len(tempPath) == ncities + 1 and i == 0: # See if a full path was made, and then update BSSF.
+						del tempPath[-1] # Take off the last value since it's a repeat.
 						self.bssf = TSPSolution(tempPath)
+						count += 1
 					else:
-						heapq.heappush(heap, (tempBound, i, tempMatrix, tempPath))
+						tempID = str(uuid.uuid1()) # Add to the queue.
+						hashMap[tempID] = (i, tempMatrix.copy(), tempPath)
+						heapq.heappush(heap, (tempBound, tempID))
+						if maxHeapSize < len(heap):
+							maxHeapSize = len(heap)
 				else:
 					pruned += 1
 		
 		end_time = time.time()
+  
+		while heap:
+			bound, ID = heapq.heappop(heap)
+			if bound > self.bssf.cost:
+				pruned += 1
 			
 		results['cost'] = self.bssf.cost
 		results['time'] = end_time - start_time
 		results['count'] = count
 		results['soln'] = self.bssf
-		results['max'] = None
-		results['total'] = None
+		results['max'] = maxHeapSize
+		results['total'] = total
 		results['pruned'] = pruned
 		return results
 
